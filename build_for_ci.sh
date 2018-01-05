76f22f05d3aa24f2519aeb827a0cc714c6581fdb
squalfof <squalfof@gmail.com>
2018-01-05 17:32:15 +0800

    update

--- build_for_ci.sh
+++ build_for_ci.sh
@@ -52,7 +52,18 @@ rm -rf __MYSQL_INSTALL_DIR__
 rm -rf packageRoot
 rm -rf databaseDir
 make install -j8
-echo "-- install done"
+sh -x ../post-make-install
+echo "-- make done"
 
+# mv package to ci_build
+#rm -rf $BASE/mysql-package
+#rm -rf $BASE/mysql-data
 
+#rm -rf packageRoot
+#mv __MYSQL_INSTALL_DIR__ packageRoot
+
+#sh -x packageRoot/init.sh prepare databaseDir
+#sh -x packageRoot/init.sh init
+
+#echo "-- install done"
 
